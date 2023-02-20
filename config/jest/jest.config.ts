f87a893de597631448a189869c7eafe8a6f5f264
AlinaSh96 <alina30081996@gmail.com>
2023-02-20 18:59:33 +0300

    add react testing lib

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,19 +3,14 @@
  * https://jestjs.io/docs/configuration
  */
 
-export default {
-    // All imported modules in your tests should be mocked automatically
+import path from 'path';
 
+export default {
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
         '\\\\node_modules\\\\',
     ],
-    // An array of directory names to be searched recursively up from the requiring module's location
-    moduleDirectories: [
-        'node_modules',
-    ],
-    // An array of file extensions your modules use
     moduleFileExtensions: [
         'js',
         'jsx',
@@ -24,23 +19,22 @@ export default {
         'json',
         'node',
     ],
-    // The root directory that Jest should scan for tests and modules within
-    rootDir: '../../',
-    // The glob patterns Jest uses to detect test files
+    moduleDirectories: [
+        'node_modules',
+    ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
     testMatch: [
+        // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
-
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\alina.shibaeva.ITGLOBAL\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
-
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
@@ -87,6 +81,10 @@ export default {
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
+    // An array of directory names to be searched recursively up from the requiring module's location
+
+    // An array of file extensions your modules use
+
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     // moduleNameMapper: {},
 
@@ -120,6 +118,8 @@ export default {
     // Automatically restore mock state and implementation before every test
     // restoreMocks: false,
 
+    // The root directory that Jest should scan for tests and modules within
+
     // A list of paths to directories that Jest should use to search for files in
     // roots: [
     //   "<rootDir>"
@@ -147,6 +147,8 @@ export default {
 
     // Adds a location field to test results
     // testLocationInResults: false,
+
+    // The glob patterns Jest uses to detect test files
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
