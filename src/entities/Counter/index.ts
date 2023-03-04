5247da863fa145ac59ac265e3b2bba4feae38391
AlinaSh96 <alina30081996@gmail.com>
2023-03-04 22:39:40 +0300

    fix import

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,6 +1,6 @@
 import { counterReducer } from './model/slice/CounterSlice';
 import { CounterSchema } from './model/types/counterSchema';
-import Counter from './ui/Counter';
+import { Counter } from './ui/Counter';
 
 export {
     counterReducer as CounterReducer,
